464aec046c009b4aaa24649f4515f5de31616df3
Ihor Khokhlov <eld2303@gmail.com>
2021-12-08 01:05:54 +0200

    dbg lint

--- js/airports.js
+++ js/airports.js
@@ -1,4 +1,78 @@
-$(function() {
-    'use strict';
+$(() => {
+
+    "use strict";
+
+    const map = L.map(
+        "map",
+        {
+            "center": [
+                57.75,
+                -152.49
+            ],
+            "zoom": 13
+        }
+    );
+
+    L.tileLayer(
+        "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png",
+        {
+            "attribution": "&copy; <a href=\"https://openstreetmap.org/copyright\">OpenStreetMap contributors</a>",
+            "maxZoom": 15,
+            "minZoom": 3
+        }
+    ).addTo(map);
+
+    let ICAO,
+        airports;
+
+    var addMarkers = function (markersData) {
+
+        $.each(
+            markersData,
+            (key, markerData) => {
+
+                L.marker(
+                    [
+                        markerData.lat,
+                        markerData.long
+                    ],
+                    {"title": markerData.icao}
+                ).addTo(map).on(
+                    "click",
+                    onMarkerClick
+                );
+
+            }
+        );
+
+    }
+
+    function onMarkerClick() {
+
+        const code = this.options.title;
+
+        console.log(code);
+
+    }
+
+    $.getJSON(
+        "../data/icao.json",
+        (data) => {
+
+            ICAO = data;
+
+            addMarkers(ICAO);
+
+        }
+    );
+
+    $.getJSON(
+        "../data/airports.json",
+        (data) => {
+
+            airports = data;
+
+        }
+    );
 
 });
